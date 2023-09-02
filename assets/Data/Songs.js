a632271d24e80e2d640b6f6ba6af1b75779ef065
Maximiliano <maxjarzy@gmail.com>
2023-09-02 02:00:04 -0300

    Configurando RNTP, arreglando nombres de medios, genero cliente de desarrollo

--- assets/Data/Songs.js
+++ assets/Data/Songs.js
@@ -5,245 +5,245 @@ const songs =
         title: 'En la intimidad',
         artist: 'Emilia Mernes ft Callejero Fino',
         genre: 'Cumbia',
-        url: '../../Songs/Genres/Cumbia/En_la_intimidad.mp3'
+        url: require('../../assets/Songs/Genres/Cumbia/En_la_intimidad.mp3')
     },
     {
         id: 2,
         title: 'M.A Remix',
         artist: 'BM, Callejero Fino, La Joaqui, Lola Indigo',
         genre: 'Cumbia',
-        url: '../../Songs/Genres/Cumbia/MA_remix.mp3'
+        url: require('../../assets/Songs/Genres/Cumbia/MA_remix.mp3')
     },
     {
         id: 3 ,
         title: 'Un Finde',
         artist: 'Big One, FMK, Ke personajes',
         genre: 'Cumbia',
-        url:'../../Songs/Genres/Cumbia/Un_finde.mp3'
+        url:require('../../assets/Songs/Genres/Cumbia/Un_finde.mp3')
     },
     {
         id: 4,
         title: 'A romper la disco',
         artist: 'Tommy Viera, Daddy Yankee',
         genre: 'Reggaeton',
-        url: '../../Songs/Genres/Reggaetón/A_romper_la_disco.mp3'
+        url: require('../../assets/Songs/Genres/Reggaetón/A_romper_la_disco.mp3')
     },
     {
         id: 5,
         title: 'Finge que me amas',
         artist: 'Tony Dize, Don Omar',
         genre: 'Reggaeton',
-        url: '../../Songs/Genres/Reggaetón/Finge_que_me_amas.mp3'
+        url: require('../../assets/Songs/Genres/Reggaetón/Finge_que_me_amas.mp3')
     },
     {
         id: 6,
         title: 'Flow HP',
         artist: 'Don Omar, Residente',
         genre: 'Reggaeton',
-        url: '../../Songs/Genres/Reggaetón/Flow_HP.mp3'
+        url: require('../../assets/Songs/Genres/Reggaetón/Flow_HP.mp3')
     },
     {
         id: 7,
         title: 'No es cumpa mía',
         artist: 'Dady Yankee',
         genre: 'Reggaeton',
-        url: '../../Songs/Genres/Reggaetón/No_es_culpa_mia.mp3'
+        url: require('../../assets/Songs/Genres/Reggaetón/No_es_culpa_mia.mp3')
     },
     {
         id: 8,
         title: 'RX',
         artist: 'Don Omar, Kendo, Syko',
         genre: 'Reggaeton',
-        url: '../../Songs/Genres/Reggaetón/RX.mp3'
+        url: require('../../assets/Songs/Genres/Reggaetón/RX.mp3')
     },
     {
         id: 9,
         title: 'La balada del diablo y la muerte (Encuentro en el estudio version)',
         artist: 'La Renga',
         genre: 'Rock',
-        url: '../../Songs/Genres/Rock/La_balada_del_diablo_y_la_muerte_(Estudio_version).mp3'
+        url: require('../../assets/Songs/Genres/Rock/La_balada_del_diablo_y_la_muerte_(Estudio_version).mp3')
     },
     {
         id: 10,
         title: 'Como Alí',
         artist: 'Los piojos',
         genre: 'Rock',
-        url: '../../Songs/Genres/Rock/Como_Alí.mp3'
+        url: require('../../assets/Songs/Genres/Rock/Como_Alí.mp3')
     },
     {
         id: 11,
         title: 'Cross road blues',
         artist: 'Robert Johnson',
         genre: 'Blues',
-        url: '../../Songs/Genres/Blues/Cross_road_blues.mp3'
+        url: require('../../assets/Songs/Genres/Blues/Cross_road_blues.mp3')
     },
     {
         id: 12,
         title: 'Hoochie coochie man',
         artist: 'Muddy Waters',
         genre: 'Blues',
-        url: '../../Songs/Genres/Blues/Hoochie_coochie_man.mp3'
+        url: require('../../assets/Songs/Genres/Blues/Hoochie_coochie_man.mp3')
     },
     {
         id: 13,
         title: 'Four Seasons',
         artist: 'Antonio Vivaldi',
         genre: 'Clasic',
-        url: '../../Songs/Genres/Clasic/Four_seasons.mp3'
+        url: require('../../assets/Songs/Genres/Clasic/Four_seasons.mp3')
     },
     {
         id: 14,
         title: 'Für Elise',
         artist: 'Ludwig van Beethoven',
         genre: 'Clasic',
-        url: '../../Songs/Genres/Clasic/Für_Elise.mp3'
+        url: require('../../assets/Songs/Genres/Clasic/Für_Elise.mp3')
     },
     {
         id: 15,
         title: 'Cruise',
         artist: 'Florida Georgia Line',
         genre: 'Country',
-        url: '../../Songs/Genres/Country/Cruise.mp3'
+        url: require('../../assets/Songs/Genres/Country/Cruise.mp3')
     }, 
     {
         id: 16,
         title: 'Friends in low places',
         artist: 'Garth Brooks',
         genre: 'Country',
-        url: '../../Songs/Genres/Country/Friends_In_low_places.mp3'
+        url: require('../../assets/Songs/Genres/Country/Friends_In_low_places.mp3')
     },
     {
         id: 17,
         title: 'Firs of the year',
         artist: 'Skrillex',
         genre: 'Electronic',
-        url: '../../Songs/Genres/Electronic/Firs_of_the_year.mp3'
+        url: require('../../assets/Songs/Genres/Electronic/First_of_the_year.mp3')
     },
     {
         id: 18,
         title: 'Lean on',
         artist: 'Major Lazer & DJ Snake',
         genre: 'Electronic',
-        url: '../../Songs/Genres/Electronic/Lean_on.mp3'
+        url: require('../../assets/Songs/Genres/Electronic/Lean_on.mp3')
     },
     {
         id: 19,
         title: 'Make it bun dem',
         artist: 'Skrillex & Damian Marley',
         genre: 'Electronic',
-        url: '../../Songs/Genres/Electronic/Make_it_bun_dem.mp3'
+        url: require('../../assets/Songs/Genres/Electronic/Make_it_bun_dem.mp3')
     },
     {
         id: 20,
         title: 'The sound of silence',
         artist: 'Simon and Garfunkel',
         genre: 'Folk',
-        url: '../../Songs/Genres/Folk/The_sound_of_silence.mp3'
+        url: require('../../assets/Songs/Genres/Folk/The_sound_of_silence.mp3')
     },
     {
         id: 21,
         title: 'Lose yourself',
         artist: 'Eminem',
         genre: 'Hip_Hop',
-        url: '../../Songs/Genres/Hip_Hop/Lose_yourself.mp3'
+        url: require('../../assets/Songs/Genres/Hip_Hop/Lose_yourself.mp3')
     },
     {
         id: 22,
         title: "You don't know",
         artist: 'Eminem ft 50 Cent, Cashis, Lloyd Banks',
         genre: 'Hip_Hop',
-        url: '../../Songs/Genres/Hip_Hop/You_dont_know.mp3'
+        url: require('../../assets/Songs/Genres/Hip_Hop/You_dont_know.mp3')
     },
     {
         id: 23,
         title: 'So what',
         artist: 'Miles Davis',
         genre: 'Jazz',
-        url: '../../Songs/Genres/Jazz/So_what.mp3'
+        url: require('../../assets/Songs/Genres/Jazz/So_what.mp3')
     },
     {
         id: 24,
         title: 'Take five',
         artist: 'Dave Brubeck',
         genre: 'Jazz',
-        url: '../../Songs/Genres/Jazz/Tave_five.mp3'
+        url: require('../../assets/Songs/Genres/Jazz/Take_five.mp3')
     },
     {
         id: 25,
         title: 'Crazy train',
         artist: 'Ozzy Osbourne',
         genre: 'Metal',
-        url: '../../Songs/Genres/Metal/Crazy_train.mp3'
+        url: require('../../assets/Songs/Genres/Metal/Crazy_train.mp3')
     },
     {
         id: 26,
         title: 'Enter sandman',
         artist: 'Metallica',
         genre: 'Metal',
-        url: '../../Songs/Genres/Metal/Enter_sandman.mp3'
+        url: require('../../assets/Songs/Genres/Metal/Enter_sandman.mp3')
     },
     {
         id: 27,
         title: 'Bad guy',
         artist: 'Billie Eilish',
         genre: 'Pop',
-        url: '../../Songs/Pop/Bad_guy.mp3'
+        url: require('../../assets/Songs/Genres/Pop/Bad_guy.mp3')
     },
     {
         id: 28,
         title: 'Shape of you',
         artist: 'Ed Sheeran',
         genre: 'Pop',
-        url: '../../Songs/Pop/Shape_of_you.mp3'
+        url: require('../../assets/Songs/Genres/Pop/Shape_of_you.mp3')
     },
     {
         id: 29,
         title: 'Someone like you',
         artist: 'Adele',
         genre: 'Pop',
-        url: '../../Songs/Pop/Someone_like_you.mp3'
+        url: require('../../assets/Songs/Genres/Pop/Someone_like_you.mp3')
     },
     {
         id: 30,
         title: 'Blinding ligths',
         artist: 'The Weeknd',
         genre: 'R&B',
-        url: '../../Songs/R&B/Blinding_lights.mp3'
+        url: require('../../assets/Songs/Genres/R&B/Blinding_lights.mp3')
     },
     {
         id: 31,
         title: 'Say so',
         artist: 'Doja Cat',
         genre: 'R&B',
-        url: '../../Songs/R&B/Say_so.mp3'
+        url: require('../../assets/Songs/Genres/R&B/Say_so.mp3')
     },
     {
         id: 32,
         title: 'I wanna love you',
         artist: 'Bob Marley',
         genre: 'Reggae',
-        url: '../../Songs/Reggae/I_wanna_love_you.mp3'
+        url: require('../../assets/Songs/Genres/Reggae/I_wanna_love_you.mp3')
     },
     {
         id: 33,
         title: 'No women no cry',
         artist: 'Bob Marley',
         genre: 'Reggae',
-        url: '../../Songs/Reggae/No_women_no_cry.mp3'
+        url: require('../../assets/Songs/Genres/Reggae/No_women_no_cry.mp3')
     },
     {
         id: 34,
         title: 'Pedro Navaja',
         artist: 'Rubén Blades',
         genre: 'Salsa',
-        url: '../../Songs/Salsa/Pedro_navaja.mp3'
+        url: require('../../assets/Songs/Genres/Salsa/Pedro_Navaja.mp3')
     },
     {
         id: 35,
         title: 'Quimbara',
         artist: 'Celia Cruz',
         genre: 'Salsa',
-        url: '../../Songs/Salsa/Quimbara.mp3'
+        url: require('../../assets/Songs/Genres/Salsa/Quimbara.mp3')
     },
 
 
--- src/Screens/Player.jsx
+++ src/Screens/Player.jsx
@@ -1,25 +1,47 @@
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-  Image,
-  ImageBackground,
-} from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
+import TrackPlayer, {
+  Capability,
+  Event,
+  State,
+  usePlaybackState,
+  useProgress,
+  useTrackPlayerEvents,
+} from "react-native-track-player";
 import {
   AntDesign,
   FontAwesome5,
   MaterialCommunityIcons,
+  Ionicons,
 } from "@expo/vector-icons";
 import { colors } from "../../assets/Colors/Colors";
 import { useSelector } from "react-redux";
 import { useGetSongByIdQuery } from "../Services/dataServices";
 import Loader from "../Components/Loader";
-import { Audio } from "expo-av";
-import { Asset } from "expo-asset";
 import { useEffect } from "react";
+import songs from "../../assets/Data/Songs";
 
 const Player = ({ navigation }) => {
+  const setUpPlayer = async () => {
+    try {
+      await TrackPlayer.setupPlayer();
+      await TrackPlayer.add(songs);
+      TrackPlayer.updateOptions({});
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const togglePayBack = async (playBackState) => {
+    const currentTrack = await TrackPlayer.getCurrentTrack();
+    if (currentTrack != null) {
+      if (playBackState == State.Paused || playBackState == State.Ready) {
+        await TrackPlayer.play();
+      } else {
+        await TrackPlayer.pause();
+      }
+    }
+  };
+  const playBackState = usePlaybackState();
+
   const songId = useSelector((state) => state.selectionReducer.value.song);
 
   const {
@@ -28,83 +50,58 @@ const Player = ({ navigation }) => {
     isError,
   } = useGetSongByIdQuery(songId);
 
-  const reproducirCancion = async (audio) => {
-    const soundObject = new Audio.Sound();
-    try {
-      const asset = Asset.fromModule(
-        require(`../../assets/Songs/Genres/${audio}`)
-      );
-      await soundObject.loadAsync(asset);
-      await soundObject.playAsync();
-    } catch (error) {
-      console.error("Error al reproducir la canción:", error);
-    }
-  };
-
   useEffect(() => {
-    if (songSelected) {
-      const { url } = songSelected;
-      reproducirCancion(url);
-    }
-  }, [songSelected]);
+    setUpPlayer();
+    return () => {
+      TrackPlayer.reset()
+    };
+  }, []);
+   
 
-  /*  
-  const onPlay = () => {};
 
-  const onNext = () => {};
-
-  const onPrevious = () => {};
-
-  const onRandom = () => {};
-
- */
   return !isLoading ? (
     <View style={styles.container}>
-      <ImageBackground
-        source={require("../../assets/Img/Backgrounds/WhiteWood.png")}
-        resizeMode="cover"
-        style={styles.background}
-      >
-        <View style={styles.info}>
-          <Image
-            source={{
-              uri: "https://static.thenounproject.com/png/1604738-200.png",
-            }}
-            style={styles.image}
-          />
-          <Text>{songSelected.title}</Text>
-          <Text>{songSelected.artist}</Text>
-        </View>
-      </ImageBackground>
+      <View style={styles.info}>
+        <Image
+          source={{
+            uri: "https://static.thenounproject.com/png/1604738-200.png",
+          }}
+          style={styles.image}
+        />
+        <Text>{}</Text>
+        <Text>{}</Text>
+      </View>
       <View style={styles.playerContainer}>
         <TouchableOpacity>
           <FontAwesome5
             name="random"
-            size={24}
+            size={25}
             color="black"
             style={styles.icon}
           />
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => TrackPlayer.skipToPrevious()}>
           <AntDesign
-            name="arrowleft"
-            size={35}
+            name="stepbackward"
+            size={25}
             color="black"
             style={styles.icon}
           />
         </TouchableOpacity>
-        <TouchableOpacity>
-          <AntDesign
-            name="playcircleo"
+        <TouchableOpacity onPress={() => togglePayBack(playBackState)}>
+          <Ionicons
+            name={
+              playBackState === State.Playing ? "md-pause" : "md-play-sharp"
+            }
             size={50}
             color="black"
             style={styles.icon}
           />
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => TrackPlayer.skipToNext()}>
           <AntDesign
-            name="arrowright"
-            size={35}
+            name="stepforward"
+            size={25}
             color="black"
             style={styles.icon}
           />
@@ -112,8 +109,9 @@ const Player = ({ navigation }) => {
         <TouchableOpacity onPress={() => navigation.navigate("ActualPlaylist")}>
           <MaterialCommunityIcons
             name="playlist-music"
-            size={35}
+            size={25}
             color="black"
+            style={styles.icon}
           />
         </TouchableOpacity>
       </View>
@@ -128,19 +126,21 @@ export default Player;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.catDarkness,
   },
   playerContainer: {
     flex: 1,
     flexDirection: "row",
     alignItems: "center",
     backgroundColor: colors.catDarkness,
-    justifyContent: "space-between",
+    justifyContent: "center",
     borderBottomColor: "white",
     borderBottomWidth: 0.5,
     padding: 5,
+    columnGap: 10
   },
   icon: {
-    margin: 10,
+    margin: 2,
   },
   info: {
     flex: 4,
